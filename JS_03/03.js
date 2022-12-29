556c81881f79fe428212fdc2f37cbd3997ee229e
okina3 <takosos@icloud.com>
2022-12-29 21:32:39 +0900

    【フロントエンド】03 変数とデータ型。の課題です。

---
+++ JS_03/03.js
@@ -0,0 +1,18 @@
+// let  a;
+// a = 10;
+// console.log(a);
+
+// let a = 10;
+// let b = 20;
+// b = a;
+// console.log(a);
+// console.log(b); 
+
+// let inputValue = prompt();
+// let x = inputValue * 2;
+// alert(x);
+
+//課題-------------------------------
+let inputValue = prompt();
+let x = inputValue * 1.1;
+alert(x);
