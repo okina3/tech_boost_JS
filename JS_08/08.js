a8563fd41690017dff38dbcbf0bb56a82cbdb1ff
okina3 <takosos@icloud.com>
2023-01-10 15:12:03 +0900

    【フロントエンド】08 関数。の課題です

---
+++ JS_08/08.js
@@ -0,0 +1,16 @@
+//関数名あり------------------------------------
+// function area($bottom, $height) {
+//    let answer = $bottom * $height / 2;
+//    return answer;
+// }
+
+// console.log(area(10, 10));
+
+
+//関数名なし------------------------------------
+let area = function ($bottom, $height) {
+   let answer = $bottom * $height / 2;
+   return answer;
+}
+
+console.log(area(10, 10));
