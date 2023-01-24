968c359fadf5d4508022590ebb8a668c0a9d2d54
okina3 <takosos@icloud.com>
2023-01-24 15:33:15 +0900

    よく分からない為飛ばす。

--- JS_17/17.js
+++ JS_17/17.js
@@ -1,36 +1,32 @@
-// // JSON形式でのデータ（文字列）
-// const json_data = '{"message":null,"results":[{"address1":"北海道","address2":"美唄市","address3":"上美唄町協和","zipcode":"0790177"},{"address1":"北海道","address2":"美唄市","address3":"上美唄町南","zipcode":"0790177"}],"status":200}';
+const apiKey = 'DEMO_KEY';
 
-// // データ型の確認
-// console.log(typeof (json_data));
-
-// // JSON形式のデータ（文字列）をJavaScriptのオブジェクトに変換
-// let obj = JSON.parse(json_data);
-
-// // データ型の確認
-// console.log(typeof (obj));
-
-// // オブジェクト型に変換した結果の出力
-// console.log(obj);
-
-
-const obj = {
-   'message': null,
-   'results': [
-      { 'address1': '北海道', 'address2': '美唄市', 'address3': '上美唄町協和', 'zipcode': '0790177' },
-      { 'address1': '北海道', 'address2': '美唄市', 'address3': '上美唄町南', 'zipcode': '0790177' }
-   ],
-   'status': 200
-};
-
-// データ型の確認
-console.log(typeof (obj));
-
-// JavaScriptのオブジェクトをJSON形式の文字列に変換
-let json_data = JSON.stringify(obj);
-
-// データ型の確認
-console.log(typeof (json_data));
-
-// 変換結果の出力
-console.log(json_data);
+$(function () {
+   $('#update').click(function (event) {
+      let param = {
+         date: $('#image-date').val(),
+         api_key: apiKey
+      };
+      console.log(param);
+      // AjaxによりWeb APIを呼び出す処理
+      $.ajax({
+         type: 'GET',                                    // GETで送信
+         data: param,                                    // リクエストパラメーター
+         url: 'https://api.nasa.gov/planetary/apod',     // URL
+         dataType: 'json',                              // データ型 JSON
+      })
+         .done(function (res) {
+            // 成功した場合
+            console.log(res.core);        // レスポンスフィールドの内容
+            $('#apod-image').attr('src', res.url);
+            $('#apod-title').html(res.title);
+            $('#apod-desc').html(res.explanation);
+            //ここに追加（課題）
+            $('#apod-copyright').html(res.copyright);
+            $('#apod-date').html(res.date);
+         })
+         .fail(function (res) {
+            // 失敗した場合
+            $('#apod').html('対象外の日付を入力したか通信エラーです<br>ブラウザをリロードしてください');
+         });
+   });
+});
