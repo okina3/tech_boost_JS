2da7bc5b23dd0afd10ab372d6dcc8b19bd588ed8
okina3 <takosos@icloud.com>
2023-01-29 21:58:53 +0900

    【フロントエンド】19 React。の課題です。

--- React/19.js
+++ React/19.js
@@ -4,18 +4,27 @@ class CountButton extends React.Component {
    constructor(props) {
       super(props);
       // 状態を保持するプロパティstate
-      this.state = { count: 0 };
+      this.state = { count: 100 };
    }
 
    //　出力処理
    render() {
+      if (this.state.count === 0) {
+         return React.createElement(
+            "button",
+            { onClick: () => this.setState({ count: 100 }) },
+            "カウント数：" + this.state.count
+         );
+      }
+
       return React.createElement(
          "button",
-         { onClick: () => this.setState({ count: this.state.count + 1 }) },
+         { onClick: () => this.setState({ count: this.state.count - 1 }) },
          "カウント数：" + this.state.count
       );
    }
 }
+
 
 // 表示される場所の要素とコンポーネントを結び付けて表示させる
 const domContainer = document.querySelector("#app");
